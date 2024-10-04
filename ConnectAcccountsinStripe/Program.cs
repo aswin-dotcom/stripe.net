8e7de83308b8a4a4d7df8de98b50494e2da859e8
aswin-dotcom <165123698+masini-idss@users.noreply.github.com>
2024-10-04 11:02:59 +0530

    connected accounts crud operations

---
+++ ConnectAcccountsinStripe/Program.cs
@@ -0,0 +1,49 @@
+﻿using Stripe;
+//using Stripe.FinancialConnections;
+using Stripe.Forwarding;
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        StripeConfiguration.ApiKey = "sk_test_51Q24jtI34JL1OXQ341cmz7yzmhVsy1pmsZpz69zsmvtE2ys9peYZsitGxHjAWVpX3thhPf1Xsl4cpXIguKn8g42400eqkTgVn9"; // Replace with your actual Stripe secret key
+
+        var options = new AccountCreateOptions
+        {
+            Type = "custom",
+            Country = "US",
+            Email = "email@example.com",
+            BusinessType = "individual",
+            Individual = new AccountIndividualOptions
+            {
+                FirstName = "John",
+                LastName = "Doe",
+            },
+
+            //In Stripe,
+            //capabilities are used to define and control what actions a connected account is allowed to perform.When creating or updating a connected account,
+            //you need to request specific capabilities depending on your business model and the type of operations you want the account to be able to perform(e.g., making charges, receiving payouts, etc.).
+
+            Capabilities = new AccountCapabilitiesOptions
+            {
+                Transfers = new AccountCapabilitiesTransfersOptions
+                {
+                    Requested = true,
+                },
+                CardPayments = new AccountCapabilitiesCardPaymentsOptions
+                {
+                    Requested = true,
+                },
+            },
+        };
+
+        var service = new AccountService();
+        Console.WriteLine(
+        service.Create(options));
+    }
+   
+}
---
+++ ConnectAccountUpdateinstripe/Program.cs
@@ -0,0 +1,41 @@
+﻿using Stripe;
+
+class main
+{
+    static void Main (String[] args)
+    {
+        StripeConfiguration.ApiKey = "sk_test_51Q24jtI34JL1OXQ341cmz7yzmhVsy1pmsZpz69zsmvtE2ys9peYZsitGxHjAWVpX3thhPf1Xsl4cpXIguKn8g42400eqkTgVn9";
+
+        //add additional details in the metadate  of the particular connected account 
+
+        var options = new AccountUpdateOptions
+        {
+            Metadata = new Dictionary<string, string> { { "aswin", ".net developer" } },
+        };
+        var service = new AccountService();
+        Console.WriteLine(
+        service.Update("acct_1Q63PGICEEM4gDIc", options));
+
+        //to retrive  the connected account object
+        Console.WriteLine(service.Get("acct_1Q63PGICEEM4gDIc"));
+
+
+        // list all connected account to your platform
+        var list = new AccountListOptions { Limit = 20 };
+        StripeList<Account> accounts = service.List(list);
+        //returns array of objects
+        Console.WriteLine(accounts);
+
+
+        //delete the connectedaccount
+        Console.WriteLine(service.Delete("acct_1Q63KtIA7ejPFQGl"));
+
+
+        //reject the account   "disabled_reason": "rejected.fraud", just disable the account
+        var reject = new AccountRejectOptions { Reason = "fraud" };
+        Console.WriteLine(service.Reject("acct_1Q258lRDPLnwLFh1", reject));
+        
+
+
+    }
+}
